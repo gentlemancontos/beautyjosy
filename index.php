bb8eaba19a65343b631e9c6e8e3583e7c9665c28
gentlemancontos <101525646+gentlemancontos@users.noreply.github.com>
2022-03-13 17:36:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Josy Beauty</title>
+</head>
+<body>
+    <header>
+    <h1>Josy Vieira Beauty</h1>
+</header>
+
+<main>
+<article>
+
+<h2>Conheça nossos produtos/serviços:</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+
+
+</article>
+
+<aside>
+    <h3>Sobre o salão:</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+
+
+
+
+</aside>
+
+
+</main>
+
+<footer>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Labore doloribus earum ex repudiandae doloremque possimu
+     s unde corrupti non tenetur eum, sunt dicta fuga tempora 
+     quos illo quibusdam molestias corporis odit!</p>
+
+</footer>
+
+
+</body>
+</html>
